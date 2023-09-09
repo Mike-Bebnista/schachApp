c998d8fc82127970bfe884f08052f02c488c639e
Mike <mike.bebnista@stud.hs-bochum.de>
2023-09-09 14:07:55 +0200

    gameId überarbeitet

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const io = require('socket.io')(httpServer, {
 
 
 let savedBoard = "[[1,[\"Rook\",\"Black\"]],[2,[\"Knight\",\"Black\"]],[3,[\"Bishop\",\"Black\"]],[4,[\"Queen\",\"Black\"]],[5,[\"King\",\"Black\"]],[6,[\"Bishop\",\"Black\"]],[7,[\"Knight\",\"Black\"]],[8,[\"Rook\",\"Black\"]],[9,[\"Pawn\",\"Black\"]],[10,[\"Pawn\",\"Black\"]],[11,[\"Pawn\",\"Black\"]],[12,[\"Pawn\",\"Black\"]],[13,[\"Pawn\",\"Black\"]],[14,[\"Pawn\",\"Black\"]],[15,[\"Pawn\",\"Black\"]],[16,[\"Pawn\",\"Black\"]],[49,[\"Pawn\",\"White\"]],[50,[\"Pawn\",\"White\"]],[51,[\"Pawn\",\"White\"]],[52,[\"Pawn\",\"White\"]],[53,[\"Pawn\",\"White\"]],[54,[\"Pawn\",\"White\"]],[55,[\"Pawn\",\"White\"]],[56,[\"Pawn\",\"White\"]],[57,[\"Rook\",\"White\"]],[58,[\"Knight\",\"White\"]],[59,[\"Bishop\",\"White\"]],[60,[\"Queen\",\"White\"]],[61,[\"King\",\"White\"]],[62,[\"Bishop\",\"White\"]],[63,[\"Knight\",\"White\"]],[64,[\"Rook\",\"White\"]]]";
-let savedGameState = {"board":{},"active":"White","history":[{"count":0,"from":0,"to":0,"action":null,"state":{}}],"availableMoves":[{},{}],"selectedSquare":{}};
+let savedGameState = { "board": {}, "active": "White", "history": [{ "count": 0, "from": 0, "to": 0, "action": null, "state": {} }], "availableMoves": [{}, {}], "selectedSquare": {} };
 let savedTime
 
 io.on("connection", (socket) => {
@@ -21,7 +21,7 @@ io.on("connection", (socket) => {
             savedTime = performance.now()
             savedGameState = gameState;
             savedBoard = gameStateBoard;
-            io.emit('gameStateVomSocket', { savedGameState, savedBoard });
+            io.to(gameId).emit('gameStateVomSocket', { savedGameState, savedBoard });
             savedTime = performance.now() - savedTime
             console.log(performance.now())
             console.log('Zeit zum senden gebraucht ' + savedTime)
@@ -30,8 +30,8 @@ io.on("connection", (socket) => {
     socket.on('newGame', ({ gameId }) => {
         console.log('Jemand hat New Game gedrückt')
         let savedBoard = "[[1,[\"Rook\",\"Black\"]],[2,[\"Knight\",\"Black\"]],[3,[\"Bishop\",\"Black\"]],[4,[\"Queen\",\"Black\"]],[5,[\"King\",\"Black\"]],[6,[\"Bishop\",\"Black\"]],[7,[\"Knight\",\"Black\"]],[8,[\"Rook\",\"Black\"]],[9,[\"Pawn\",\"Black\"]],[10,[\"Pawn\",\"Black\"]],[11,[\"Pawn\",\"Black\"]],[12,[\"Pawn\",\"Black\"]],[13,[\"Pawn\",\"Black\"]],[14,[\"Pawn\",\"Black\"]],[15,[\"Pawn\",\"Black\"]],[16,[\"Pawn\",\"Black\"]],[49,[\"Pawn\",\"White\"]],[50,[\"Pawn\",\"White\"]],[51,[\"Pawn\",\"White\"]],[52,[\"Pawn\",\"White\"]],[53,[\"Pawn\",\"White\"]],[54,[\"Pawn\",\"White\"]],[55,[\"Pawn\",\"White\"]],[56,[\"Pawn\",\"White\"]],[57,[\"Rook\",\"White\"]],[58,[\"Knight\",\"White\"]],[59,[\"Bishop\",\"White\"]],[60,[\"Queen\",\"White\"]],[61,[\"King\",\"White\"]],[62,[\"Bishop\",\"White\"]],[63,[\"Knight\",\"White\"]],[64,[\"Rook\",\"White\"]]]";
-        let savedGameState = {"board":{},"active":"White","history":[{"count":0,"from":0,"to":0,"action":null,"state":{}}],"availableMoves":[{},{}],"selectedSquare":{}};
-        io.emit('gameStateVomSocket', { savedGameState, savedBoard });
+        let savedGameState = { "board": {}, "active": "White", "history": [{ "count": 0, "from": 0, "to": 0, "action": null, "state": {} }], "availableMoves": [{}, {}], "selectedSquare": {} };
+        io.to(gameId).emit('gameStateVomSocket', { savedGameState, savedBoard });
     })
 });
 
